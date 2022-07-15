8402061b7df7b55de4db20eca5d59b0a1105193e
IcyLux <Lu.Sollwedel95@gmail.com>
2022-07-15 15:36:47 +0200

    Test

--- uniparcity/lib/planninglist.dart
+++ uniparcity/lib/planninglist.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:uniparcity/profil.dart';
 
 import 'CustomBottomNavBar.dart';
+// Hi
 
 class PlanningList extends StatelessWidget {
   const PlanningList({Key? key}) : super(key: key);
